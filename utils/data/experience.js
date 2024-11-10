7cfa87caeb699199cc42551b338528d994102c66
Sabita4423 <sjena4423@gmail.com>
2024-11-11 00:18:38 +0530

    first commit

---
+++ utils/data/experience.js
@@ -0,0 +1,14 @@
+export const experiences = [
+  {
+    id: 1,
+    title: 'Software Engineer',
+    company: "Aashdit Technologies",
+    duration: "(Oct 2021 - Present)"
+  },
+  {
+    id: 3,
+    title: "Self Employed",
+    company: "Code and build something in everyday.",
+    duration: "(Jan 2018 - Present)"
+  }
+]
---
+++ utils/data/personal-data.js
@@ -0,0 +1,17 @@
+export const personalData = {
+  name: "SABITA JENA",
+  profile: '/profile.png',
+  designation: "Software Developer",
+  description: "My name is SABITA JENA. I am a professional and enthusiastic programmer in my daily life. I am a quick learner with a self-learning attitude. I love to learn and explore new technologies and am passionate about problem-solving. I love almost all the stacks of web application development and love to make the web more open to the world. My core skill is based on JavaScript and I love to do most of the things using JavaScript. I am available for any kind of job opportunity that suits my skills and interests.",
+  email: 'sjena4423@gmail.com',
+  phone: '+916371203209',
+  address: 'BBSR, Odisha - 751024 ',
+  github: 'https://github.com/Sabita4423',
+  facebook: 'https://www.facebook.com/',
+  linkedIn: 'https://www.linkedin.com/in/sabita-jena-578228194/',
+  twitter: 'https://twitter.com/',
+  stackOverflow: 'https://stackoverflow.com/',
+  leetcode: "https://leetcode.com/",
+  devUsername: "7",
+  resume: "https://drive.google.com/file/d/1kk2mtP5odoIWG3-1Uf9BTChrGmmlTfpO/view?usp=sharing"
+}
